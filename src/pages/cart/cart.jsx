a2c3c7177cdad65a3018a7834a97a61cf4852724
Elgun837 <elgun.xalilov@bk.ru>
2023-04-11 15:40:08 +0400

    create-cart

---
+++ src/pages/cart/cart.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react'
+import { PRODUCTS } from '../../products'
+import { ShopContext } from '../../contex/shop-context'
+import { CartItem } from './cart-item';
+import "./cart.css";
+export const Cart = () => {
+
+  const {cartItems} = useContext(ShopContext);
+  return (
+    <div className='cart'>
+        <div>
+            <h1>Your cart items</h1>
+        </div>
+        <div className="cart">
+            {PRODUCTS.map((product => {
+                if (cartItems[product.id] !== 0){
+                    return (
+                        <CartItem data={product} key={product.id}/>
+                    )
+                }
+            }))}
+        </div>
+    </div>
+  )
+}
---
+++ src/products.js
@@ -0,0 +1,60 @@
+import product1 from './assets/Product1.jpg'
+import product2 from './assets/Product2.png'
+import product3 from './assets/Product3.png'
+import product4 from './assets/Product4.png'
+import product5 from './assets/Product5.png'
+import product6 from './assets/Product6.png'
+import product7 from './assets/Product7.png'
+import product8 from './assets/Product8.png'
+
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Jacket",
+        price: 199.0,
+        productImage: product1
+    },
+    {
+        id: 2,
+        productName: "Hp Pavilion",
+        price: 999.0,
+        productImage: product2
+    },
+    {
+        id: 3,
+        productName: "RTX 3080 TI",
+        price: 1299.0,
+        productImage: product3
+    },
+    {
+        id: 4,
+        productName: "Hyper X Ram 8gb",
+        price: 99.0,
+        productImage: product4
+    },
+    {
+        id: 5,
+        productName: "Sandisk Ultra SSD 480gb",
+        price: 49.0,
+        productImage: product5
+    },
+    {
+        id: 6,
+        productName: "Intel Core-i7 cpu 10700",
+        price: 799.0,
+        productImage: product6
+    },
+    {
+        id: 7,
+        productName: "Iphone X 512gb",
+        price: 999.0,
+        productImage: product7
+    },
+    {
+        id: 8,
+        productName: "Canon 510D",
+        price: 899.0,
+        productImage: product8
+    },
+]
